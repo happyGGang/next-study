9ef3b446bc4e6f83f64cd2052828cc6ddcdeb785
ykkang <ykkang@ocube.co.kr>
2023-04-19 10:02:00 +0900

    create Navi btn

--- blog/pages/posts/first-post.js
+++ blog/pages/posts/first-post.js
@@ -1,8 +1,8 @@
-export default function FirstPage() {
+export default function FirstPost() {
   return (
     <div>
-      <h1>first page</h1>
-      <h2>첫번째 페이지</h2>
+      <h1>first post</h1>
+      <h2>첫번째 포스트 페이지</h2>
     </div>
   )
 }
