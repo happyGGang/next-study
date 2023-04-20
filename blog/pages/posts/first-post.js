b706b9fc167e22a75a28e7d6d5156c00e1d149e2
ykkang <ykkang@ocube.co.kr>
2023-04-20 17:40:44 +0900

    create blog, styling blog

--- blog/pages/posts/first-post.js
+++ blog/pages/posts/first-post.js
@@ -1,8 +1,13 @@
+import Head from 'next/head'
+import Layout from '../../components/Layout'
+
 export default function FirstPost() {
   return (
-    <div>
-      <h1>first post</h1>
-      <h2>첫번째 포스트 페이지</h2>
-    </div>
+    <Layout>
+      <Head>
+        <title>첫번째 글</title>
+      </Head>
+      <h1>첫번째 글</h1>
+    </Layout>
   )
 }
---
+++ blog/pages/posts/second-post.js
@@ -0,0 +1,8 @@
+export default function SecondPost() {
+  return (
+    <div>
+      <h1>second post</h1>
+      <h2>두번째 포스트 페이지</h2>
+    </div>
+  )
+}
